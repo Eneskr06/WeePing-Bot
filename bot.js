4875874fb5800f031faa18136a6c65d7738d925e
Eneskr06 <76180810+Eneskr06@users.noreply.github.com>
2021-10-17 14:44:52 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,699 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const chalk = require("chalk");
+const moment = require("moment");
+const ms = require("ms");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const fs = require('fs');
+const replaceOnce = require("replace-once");
+require("./util/eventLoader.js")(client);
+const db = require("quick.db");
+const queue = new Map();
+const YouTube = require("simple-youtube-api");
+const superagent = require("superagent");
+const ytdl = require("ytdl-core");
+const ayarlar = require("./ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+////-----------------------------\\\\\\\\\
+
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+
+client.on("userUpdate", async(Revenge, NYKS) => {
+  
+if(Revenge.avatarURL === NYKS.avatarURL) return;
+let RevengePP = NYKS.avatarURL.split("?")[0]
+
+const Kanal = client.channels.filter(Revenge => Revenge.name === 'random-gif' && Revenge.type === 'text').array().forEach(Nucceteere => {
+if (RevengePP.endsWith('.gif')) {
+Nucceteere.send(new Discord.Attachment(RevengePP))
+}
+const Kanal2 = client.channels.filter(Revenge => Revenge.name === 'random-pp' && Revenge.type === 'text').array().forEach(Revenge => {
+if (!RevengePP.endsWith('.gif')) {
+Revenge.send(new Discord.Attachment(RevengePP))
+}
+})
+})
+})
+
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+
+//ModLog Baş
+
+client.on("messageDelete", async message => {
+  if (message.author.bot || message.channel.type == "dm") return;
+
+  let log = message.guild.channels.cache.get(
+    await db.fetch(`log_${message.guild.id}`)
+  );
+
+  if (!log) return;
+
+  const embed = new Discord.MessageEmbed()
+
+    .setTitle(message.author.username + " | Mesaj Silindi")
+
+    .addField("Kullanıcı: ", message.author)
+
+    .addField("Kanal: ", message.channel)
+
+    .addField("Mesaj: ", "" + message.content + "");
+
+  log.send(embed);
+});
+
+client.on("messageUpdate", async (oldMessage, newMessage) => {
+  let modlog = await db.fetch(`log_${oldMessage.guild.id}`);
+
+  if (!modlog) return;
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(oldMessage.author.username, oldMessage.author.avatarURL())
+
+    .addField("**Eylem:**", "Mesaj Düzenleme")
+
+    .addField(
+      "**Mesajın sahibi:**",
+      `<@${oldMessage.author.id}> === **${oldMessage.author.id}**`
+    )
+
+    .addField("**Eski Mesajı:**", `${oldMessage.content}`)
+
+    .addField("**Yeni Mesajı:**", `${newMessage.content}`)
+
+    .setTimestamp()
+
+    .setColor("#00ff00")
+
+    .setFooter(
+      `Sunucu: ${oldMessage.guild.name} - ${oldMessage.guild.id}`,
+      oldMessage.guild.iconURL()
+    )
+
+    .setThumbnail(oldMessage.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+client.on("channelDelete", async channel => {
+  let modlog = await db.fetch(`log_${channel.guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await channel.guild
+    .fetchAuditLogs({ type: "CHANNEL_DELETE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Kanal Silme")
+
+    .addField("**Kanalı Silen Kişi:**", `<@${entry.executor.id}>`)
+
+    .addField("**Silinen Kanal:**", `\`${channel.name}\``)
+
+    .setTimestamp()
+
+    .setColor("#00ff00")
+
+    .setFooter(
+      `Sunucu: ${channel.guild.name} - ${channel.guild.id}`,
+      channel.guild.iconURL()
+    )
+
+    .setThumbnail(channel.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("roleCreate", async role => {
+  let modlog = await db.fetch(`log_${role.guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await role.guild
+    .fetchAuditLogs({ type: "ROLE_CREATE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Rol Oluşturma")
+
+    .addField("**Rolü oluşturan kişi:**", `<@${entry.executor.id}>`)
+
+    .addField("**Oluşturulan rol:**", `\`${role.name}\` **=** \`${role.id}\``)
+
+    .setTimestamp()
+
+    .setFooter(
+      `Sunucu: ${role.guild.name} - ${role.guild.id}`,
+      role.guild.iconURL
+    )
+
+    .setColor("#00ff00")
+
+    .setThumbnail(role.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("roleDelete", async role => {
+  let modlog = await db.fetch(`log_${role.guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await role.guild
+    .fetchAuditLogs({ type: "ROLE_DELETE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Rol Silme")
+
+    .addField("**Rolü silen kişi:**", `<@${entry.executor.id}>`)
+
+    .addField("**Silinen rol:**", `\`${role.name}\` **=** \`${role.id}\``)
+
+    .setTimestamp()
+
+    .setFooter(
+      `Sunucu: ${role.guild.name} - ${role.guild.id}`,
+      role.guild.iconURL
+    )
+
+    .setColor("#00ff00")
+
+    .setThumbnail(role.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("emojiCreate", async emoji => {
+  let modlog = await db.fetch(`log_${emoji.guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await emoji.guild
+    .fetchAuditLogs({ type: "EMOJI_CREATE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Emoji Oluşturma")
+
+    .addField("**Emojiyi oluşturan kişi:**", `<@${entry.executor.id}>`)
+
+    .addField("**Oluşturulan emoji:**", `${emoji} - İsmi: \`${emoji.name}\``)
+
+    .setTimestamp()
+
+    .setColor("#00ff00")
+
+    .setFooter(
+      `Sunucu: ${emoji.guild.name} - ${emoji.guild.id}`,
+      emoji.guild.iconURL
+    )
+
+    .setThumbnail(emoji.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("emojiDelete", async emoji => {
+  let modlog = await db.fetch(`log_${emoji.guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await emoji.guild
+    .fetchAuditLogs({ type: "EMOJI_DELETE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Emoji Silme")
+
+    .addField("**Emojiyi silen kişi:**", `<@${entry.executor.id}>`)
+
+    .addField("**Silinen emoji:**", `${emoji}`)
+
+    .setTimestamp()
+
+    .setFooter(
+      `Sunucu: ${emoji.guild.name} - ${emoji.guild.id}`,
+      emoji.guild.iconURL
+    )
+
+    .setColor("#00ff00")
+
+    .setThumbnail(emoji.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("emojiUpdate", async (oldEmoji, newEmoji) => {
+  let modlog = await db.fetch(`log_${oldEmoji.guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await oldEmoji.guild
+    .fetchAuditLogs({ type: "EMOJI_UPDATE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Emoji Güncelleme")
+
+    .addField("**Emojiyi güncelleyen kişi:**", `<@${entry.executor.id}>`)
+
+    .addField(
+      "**Güncellenmeden önceki emoji:**",
+      `${oldEmoji} - İsmi: \`${oldEmoji.name}\``
+    )
+
+    .addField(
+      "**Güncellendikten sonraki emoji:**",
+      `${newEmoji} - İsmi: \`${newEmoji.name}\``
+    )
+
+    .setTimestamp()
+
+    .setColor("#00ff00")
+
+    .setFooter(
+      `Sunucu: ${oldEmoji.guild.name} - ${oldEmoji.guild.id}`,
+      oldEmoji.guild.iconURL
+    )
+
+    .setThumbnail(oldEmoji.guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("guildBanAdd", async (guild, user) => {
+  let modlog = await db.fetch(`log_${guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await guild
+    .fetchAuditLogs({ type: "MEMBER_BAN_ADD" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Yasaklama")
+
+    .addField("**Kullanıcıyı yasaklayan yetkili:**", `<@${entry.executor.id}>`)
+
+    .addField("**Yasaklanan kullanıcı:**", `**${user.tag}** - ${user.id}`)
+
+    .addField("**Yasaklanma sebebi:**", `${entry.reason}`)
+
+    .setTimestamp()
+
+    .setColor("#00ff00")
+
+    .setFooter(`Sunucu: ${guild.name} - ${guild.id}`, guild.iconURL)
+
+    .setThumbnail(guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+
+client.on("guildBanRemove", async (guild, user) => {
+  let modlog = await db.fetch(`log_${guild.id}`);
+
+  if (!modlog) return;
+
+  const entry = await guild
+    .fetchAuditLogs({ type: "MEMBER_BAN_REMOVE" })
+    .then(audit => audit.entries.first());
+
+  let embed = new Discord.MessageEmbed()
+
+    .setAuthor(entry.executor.username, entry.executor.avatarURL())
+
+    .addField("**Eylem:**", "Yasak kaldırma")
+
+    .addField("**Yasağı kaldıran yetkili:**", `<@${entry.executor.id}>`)
+
+    .addField(
+      "**Yasağı kaldırılan kullanıcı:**",
+      `**${user.tag}** - ${user.id}`
+    )
+
+    .setTimestamp()
+
+    .setColor("#00ff00")
+
+    .setFooter(`Sunucu: ${guild.name} - ${guild.id}`, guild.iconURL)
+
+    .setThumbnail(guild.iconURL);
+
+  client.channels.cache.get(modlog).send(embed);
+});
+// ModLog Son
+
+//KüfürEngel Baş
+
+const küfür = [
+  "siktir",
+  "fuck",
+  "puşt",
+  "pust",
+  "piç",
+  "sikerim",
+  "sik",
+  "yarra",
+  "yarrak",
+  "amcık",
+  "orospu",
+  "orosbu",
+  "orosbucocu",
+  "oç",
+  ".oc",
+  "ibne",
+  "yavşak",
+  "bitch",
+  "dalyarak",
+  "amk",
+  "awk",
+  "taşak",
+  "taşşak",
+  "daşşak",
+  "sikm",
+  "sikim",
+  "sikmm",
+  "skim",
+  "skm",
+  "sg"
+];
+client.on("messageUpdate", async (old, nev) => {
+  if (old.content != nev.content) {
+    let i = await db.fetch(`küfür.${nev.member.guild.id}.durum`);
+    let y = await db.fetch(`küfür.${nev.member.guild.id}.kanal`);
+    if (i) {
+      if (küfür.some(word => nev.content.includes(word))) {
+        if (nev.member.hasPermission("BAN_MEMBERS")) return;
+        //if (ayarlar.gelistiriciler.includes(nev.author.id)) return ;
+        const embed = new Discord.MessageEmbed()
+          .setColor("#00ff00")
+          .setDescription(
+            ` ${nev.author} , **Mesajını editleyerek küfür etmeye çalıştı!**`
+          )
+          .addField("Mesajı:", nev);
+
+        nev.delete();
+        const embeds = new Discord.MessageEmbed()
+          .setColor("#00ff00")
+          .setDescription(
+            ` ${nev.author} , **Mesajı editleyerek küfür etmene izin veremem!**`
+          );
+        client.channels.cache.get(y).send(embed);
+        nev.channel.send(embeds).then(msg => msg.delete({ timeout: 5000 }));
+      }
+    } else {
+    }
+    if (!i) return;
+  }
+});
+
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+  let y = await db.fetch(`küfür.${msg.member.guild.id}.kanal`);
+
+  let i = await db.fetch(`küfür.${msg.member.guild.id}.durum`);
+  if (i) {
+    if (küfür.some(word => msg.content.toLowerCase().includes(word))) {
+      try {
+        if (!msg.member.hasPermission("MANAGE_GUILD")) {
+          //  if (!ayarlar.gelistiriciler.includes(msg.author.id)) return ;
+          msg.delete({ timeout: 750 });
+          const embeds = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setDescription(
+              ` <@${msg.author.id}> , **Bu sunucuda küfür yasak!**`
+            );
+          msg.channel.send(embeds).then(msg => msg.delete({ timeout: 5000 }));
+          const embed = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setDescription(` ${msg.author} , **Küfür etmeye çalıştı!**`)
+            .addField("Mesajı:", msg);
+          client.channels.cache.get(y).send(embed);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+//KüfürEngel Son
+
+
+//Reklam Engel Baş
+
+const reklam = [
+  ".com",
+  ".net",
+  ".xyz",
+  ".tk",
+  ".pw",
+  ".io",
+  ".me",
+  ".gg",
+  "www.",
+  "https",
+  "http",
+  ".gl",
+  ".org",
+  ".com.tr",
+  ".biz",
+  "net",
+  ".rf",
+  ".gd",
+  ".az",
+  ".party",
+  ".gf",
+  ".31"
+];
+client.on("messageUpdate", async (old, nev) => {
+  if (old.content != nev.content) {
+    let i = await db.fetch(`reklam.${nev.member.guild.id}.durum`);
+    let y = await db.fetch(`reklam.${nev.member.guild.id}.kanal`);
+    if (i) {
+      if (reklam.some(word => nev.content.includes(word))) {
+        if (nev.member.hasPermission("BAN_MEMBERS")) return;
+        //if (ayarlar.gelistiriciler.includes(nev.author.id)) return ;
+        const embed = new Discord.MessageEmbed()
+          .setColor("#00ff00")
+          .setDescription(
+            ` ${nev.author} , **Mesajını editleyerek reklam yapmaya çalıştı!**`
+          )
+          .addField("Mesajı:", nev);
+
+        nev.delete();
+        const embeds = new Discord.MessageEmbed()
+          .setColor("#00ff00")
+          .setDescription(
+            ` ${nev.author} , **Mesajı editleyerek reklam yapamana izin veremem!**`
+          );
+        client.channels.cache.get(y).send(embed);
+        nev.channel.send(embeds).then(msg => msg.delete({ timeout: 5000 }));
+      }
+    } else {
+    }
+    if (!i) return;
+  }
+});
+
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+  let y = await db.fetch(`reklam.${msg.member.guild.id}.kanal`);
+
+  let i = await db.fetch(`reklam.${msg.member.guild.id}.durum`);
+  if (i) {
+    if (reklam.some(word => msg.content.toLowerCase().includes(word))) {
+      try {
+        if (!msg.member.hasPermission("MANAGE_GUILD")) {
+          //  if (!ayarlar.gelistiriciler.includes(msg.author.id)) return ;
+          msg.delete({ timeout: 750 });
+          const embeds = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setDescription(
+              ` <@${msg.author.id}> , **Bu sunucuda reklam yapmak yasak!**`
+            );
+          msg.channel.send(embeds).then(msg => msg.delete({ timeout: 5000 }));
+          const embed = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setDescription(` ${msg.author} , **Reklam yapmaya çalıştı!**`)
+            .addField("Mesajı:", msg);
+          client.channels.cache.get(y).send(embed);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+//Reklam Engel Son
+
+
+//-------------------- Sa As Sistemi --------------------//
+
+
+client.on("message", async msg => {
+ 
+ 
+  const i = await db.fetch(`ssaass_${msg.guild.id}`);
+    if (i == 'acik') {
+      if (msg.content.toLowerCase() == 'sa' || msg.content.toLowerCase() == 's.a' || msg.content.toLowerCase() == 'selamun aleyküm' || msg.content.toLowerCase() == 'sea'|| msg.content.toLowerCase() == 'selam') {
+          try {
+ 
+                  return msg.reply(
+                    'Aleyküm Selam, Hoşgeldin')
+          } catch(err) {
+            console.log(err);
+          }
+      }
+    }
+    else if (i == 'kapali') {
+   
+    }
+    if (!i) return;
+ 
+    });
+//-------------------- Sa As Sistemi --------------------//
+//-------------------- Sa As Sistemi --------------------//
+
+
+client.on('ready', () => {
+
+  // Oynuyor Kısmı
+  
+      var actvs = [
+        `${prefix}yardım ${client.guilds.cache.size} sunucuyu`,
+        `${prefix}yardım ${client.users.cache.size} Kullanıcıyı`, 
+        `${prefix}yardım`
+    ];
+    
+    client.user.setActivity(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)], { type: 'LISTENING' });
+    setInterval(() => {
+        client.user.setActivity(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)], { type: 'LISTENING'});
+    }, 15000);
+    
+  
+      console.log ('_________________________________________');
+      console.log (`Kullanıcı İsmi     : ${client.user.username}`);
+      console.log (`Sunucular          : ${client.guilds.cache.size}`);
+      console.log (`Kullanıcılar       : ${client.users.cache.size}`);
+      console.log (`Prefix             : ${ayarlar.prefix}`);
+      console.log (`Durum              : Bot Çevrimiçi!`);
+      console.log ('_________________________________________');
+    
+    });
+
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+client.login(ayarlar.token);
